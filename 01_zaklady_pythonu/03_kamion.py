d8aeda75fab41b79e0ada94ee05541f5dd819532
Vašek <vaclavdesensky1@gmail.com>
2026-07-14 07:35:29 +0200

    Lekce 2 - vstup od uživatele

---
+++ 01_zaklady_pythonu/03_kamion.py
@@ -0,0 +1,8 @@
+auto = input("Jakou značku auta řídíš? ")
+km = input("Kolik kilometrů má najeto? ")
+cil = input("Kam s ním jezdíš? ")
+
+print("Řídíš", auto, "!!!")
+print("Má najetých", km, "kilometrů!!!")
+print("Jezdíš s ním do", cil, "!!!")
+print("Přeji ti bezpečný dojezd do ", cil)
---
+++ 01_zaklady_pythonu/03_vstup.py
@@ -0,0 +1,6 @@
+jmeno = input("Jak se jmenuješ? ")
+vek = input("Kolik je ti let? ")
+
+print("Ahoj", jmeno)
+print("Je ti", vek, "let.")
+
