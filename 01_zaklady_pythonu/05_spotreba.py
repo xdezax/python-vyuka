0374851d69504a89401d51143f48f6853f63128f
Vašek <vaclavdesensky1@gmail.com>
2026-07-15 17:41:24 +0200

    Lekce 3 - kalkulačka spotřeby

--- 01_zaklady_pythonu/05_spotreba.py
+++ 01_zaklady_pythonu/05_spotreba.py
@@ -10,3 +10,12 @@ print("Spotřeba je", round(spotreba, 2),"l/100km")
 
 if spotreba < 30:
     print("Výborná spotřeba!")
+
+print()
+print("Cena nafty")
+cena = float(input("Kolik stojí litr nafty? "))
+cena_celkem = (litry*cena)
+print(f"Nafta stála celkem {cena_celkem:.2f} Kč")
+
+
+
