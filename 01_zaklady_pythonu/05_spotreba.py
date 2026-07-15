840e1a811328b403b7b3199f70f9e73fde7dd6be
ˇVašekˇ <vaclavdesensky1@gmail.com>
2026-07-15 21:31:10 +0200

    Lekce 3 - kalkulačka spotřeby

--- 01_zaklady_pythonu/05_spotreba.py
+++ 01_zaklady_pythonu/05_spotreba.py
@@ -1,22 +1,76 @@
 kilometry = float(input("Kolik kilometrů jsi ujel? "))
 litry = float(input("Kolik litrů nafty jsi natankoval? "))
+vaha1 = float(input("Kolik kilogramů průměrně vážil náklad?  "))
 cena = float(input("Kolik stojí litr nafty? "))
+vaha = float(input("Kolik kilogramů bude vážit další náklad? "))
+dalsi_cesta = float(input("Kolik kilometrů bude mít další cesta? "))
+cena1 = float(input("Kolik zaplatí za tkm? "))
 
-spotreba = (litry/kilometry)*100
+spotreba = litry/kilometry*100
+cena_celkem = litry*cena
+cena_na_km = cena_celkem/kilometry
+dalsi = dalsi_cesta*cena_na_km
+dalsi_tkm = dalsi_cesta*vaha 
+tkm = vaha1*kilometry
+sazba = cena_celkem/(vaha1*kilometry)
+sazba1 = (cena_na_km*dalsi_cesta)/(vaha*dalsi_cesta)
+naklady = (cena_na_km*dalsi_cesta)
+trzba = dalsi_tkm*cena1
+zisk = trzba -naklady 
 
 print()
 print("Výsledek")
-print("--------------------")
-print("Spotřeba je", round(spotreba, 2),"l/100km")
-
-if spotreba < 30:
-    print("Výborná spotřeba!")
-
-cena_celkem = litry*cena
-
-print("Cena nafty")
-print("--------------------")
+print("--------------------------------------------")
+print(f"Spotřeba je {spotreba:.2f} l/100km")
+print()
 print(f"Nafta stála celkem {cena_celkem:.2f} Kč")
+print()
+print(f"Cena na 1 km je {cena_na_km:.2f} Kč")
+print()
+print(f"Výkon byl {tkm:.2f} tkm")
+print()
+print(f"Sazba je {sazba:.2f} Kč/tkm")
+print()
+print(f"Další cesta bude stát {dalsi:.2f} Kč")
+print()
+print(f"Výkon na další jízdu je {dalsi_tkm:.2f} tkm")
+print()
+print(f"Sazba na další jízdu je {sazba1:.2f} Kč/tkm")
+print()
+print(f"Zisk z další jízdy je {zisk:.2f} Kč")
+print()
+print()
+if spotreba < 30:
+    print("Máš výbornou spotřebu!")
+
+elif spotreba <= 35:
+    print("Máš dobrou spotřebu.")
+    
+elif spotreba >= 50:
+    print("Šlapej na top míň kokote!!!")   
+else:
+    print("Máš vysokou spotřebu!!")
+print()
+print()
+if zisk <= 0:
+    print("Na téhle cestě proděláš!")
+
+elif zisk <=5000:
+    print("Zisk z téhle cesty je nízký.")
+
+elif zisk <= 15000:
+    print("Zisk z téhle cesty je dobrý.")    
+
+else:
+    print("Super kšeft!!!")    
+print()
+print()
+print()
+print("Šťastnou cestu.")    
+
+
+
+
 
 
 
